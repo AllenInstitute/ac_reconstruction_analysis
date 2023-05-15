2ca6b88241cf5fe1d308c9d564d18928f91560cf
Kevin Takasaki <kevint@alleninstitute.org>
2023-05-15 11:39:38 -0700

    added write_swc_state module

---
+++ acanalysis/write_swc_state.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 11 10:45:34 2023
+
+@author: kevint
+"""
+
+import pathlib
+import argschema
+import json
+import zarr
+import numpy as np
+
+
+def get_zarr_params(zpath):
+    zf = zarr.open(zpath)
+    attrs = zf.attrs.asdict()
+    grptr = attrs['multiscales'][0]['coordinateTransformations'][0]['translation']
+    mipsc = attrs['multiscales'][0]['datasets'][0]['coordinateTransformations'][0]['scale']
+    params = {
+        "scale": [mipsc[i] for i in range(2, 5)],
+        "translation": [int(grptr[i]/mipsc[i]) for i in range(2, 5)]
+    }
+    return params
+
+
+def get_scale_dims(zparams):
+    sc = zparams["scale"]
+    scaleDims = {
+        "z": [sc[0], "um"],
+        "y": [sc[1], "um"],
+        "x": [sc[2], "um"]
+    }
+    return scaleDims
+
+
+def get_tr_matrix(zparams):
+    trList = zparams["translation"]
+    trM = [
+        [
+            1,
+            0,
+            0,
+            trList[0]
+        ],
+        [
+            0,
+            1,
+            0,
+            trList[1]
+        ],
+        [
+            0,
+            0,
+            1,
+            trList[2]
+        ]
+    ]
+    return trM
+
+
+def create_seg_layer(precompDir, zparams):
+    scaleDims = get_scale_dims(zparams)
+    trMatrix = get_tr_matrix(zparams)
+    slayer = {
+        "type": "segmentation",
+        "source": {
+            "url": precompDir,
+            "transform": {
+                "matrix": trMatrix,
+                "outputDimensions": scaleDims,
+                "inputDimensions": scaleDims
+            }
+        },
+        "tab": "source",
+        "segments": [],
+        "name": "skeletons"
+    }
+    return slayer
+
+
+def write_swc_state(zarrFile, precompDir, outputFile):
+    zpath = pathlib.Path(zarrFile)
+    if zpath.exists():
+        zparams = get_zarr_params(zpath)
+        slayer = create_seg_layer(precompDir, zparams)
+        with open(outputFile, "w+") as f:
+            json.dump(slayer, f)
+
+
+class WriteSwcNgStateInputParameters(argschema.ArgSchema, argschema.schemas.DefaultSchema):
+    zarr_file = argschema.fields.Str(required=True)
+    precomputed_dir = argschema.fields.Str(required=True)
+    output_file = argschema.fields.Str(required=True)
+
+
+class WriteSwcNgStateParser(argschema.ArgSchemaParser):
+    default_schema = WriteSwcNgStateInputParameters
+
+    def run(self):
+        write_swc_state(
+            self.args["zarr_file"],
+            self.args["precomputed_dir"],
+            self.args["output_file"])
+
+
+if __name__ == "__main__":
+    mod = WriteSwcNgStateParser()
+    mod.run()
