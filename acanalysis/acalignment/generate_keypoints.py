77c8b1afb4463d358ec1facbcd6fcf2a80a4b136
Kevin Takasaki <kevint@alleninstitute.org>
2023-09-25 19:03:12 -0700

    bug fixing

--- acanalysis/acalignment/generate_keypoints.py
+++ acanalysis/acalignment/generate_keypoints.py
@@ -89,7 +89,7 @@ def filter_surface_keypoints(keypts,distance=0,ori=None,surf_map=None,roi_coords
                         print(kp.location)
                     KeyPtList.append(kp)
     else:
-        KeyPtList = keypts
+        KeyPtList = [kp for kp in keypts if not kp.vector is None]
     if surf_map is None:
         print("Surface map not provided: defaulting to extremal node")
         hlist = numpy.array([keypt.location[0] for keypt in KeyPtList])
